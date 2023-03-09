0a856aa0d1be5857ff7fde0cfffa29ae4bb4ad96
ar.kuzmin <ar.kuzmin@yandex.ru>
2023-03-09 16:27:34 +0700

    Initial commit

---
+++ src/main/Java/org/example/HelloController.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "Hello_world";
+    }
+}
